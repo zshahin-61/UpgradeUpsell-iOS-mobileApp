c205fdde7853dd5b0cb1a9f096dcd2e28aeab251
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-04 22:46:46 -0400

    update rp41

--- UpgradeUpsell/Views/Realtor/HomeView_Realtor.swift
+++ UpgradeUpsell/Views/Realtor/HomeView_Realtor.swift
@@ -16,6 +16,6 @@ struct HomeView_Realtor: View {
     
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
