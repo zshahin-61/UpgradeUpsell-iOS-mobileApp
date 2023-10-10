45e0bf23fbfd0540f43626ea3e1f1784ebc371de
zshahin-61 <zshahin.61@gmail.com>
2023-10-10 14:19:06 -0400

    Listview

--- UpgradeUpsell/Models/RenovateProject.swift
+++ UpgradeUpsell/Models/RenovateProject.swift
@@ -1,9 +1,7 @@
-
-
 import Foundation
 import FirebaseFirestoreSwift
 
-struct RenovateProject: Codable , Identifiable {
+struct RenovateProject: Codable, Identifiable {
     @DocumentID var id: String? = UUID().uuidString
     var title: String
     var description: String
@@ -18,30 +16,32 @@ struct RenovateProject: Codable , Identifiable {
     var status: String
     var startDate: Date
     var endDate: Date
-    //let contactInfo: ContactInfo
+    var numberOfBedrooms: Int
+    var numberOfBathrooms: Int
+    var propertyType: String
+    var squareFootage: Double
+    var isFurnished: Bool
     var createdDate: Date
     var updatedDate: Date
     var favoriteCount: Int
     var realtorID: String
-    //let buySuggestions: [BuySuggestion]
-    //let selectedBuySuggestionID: String
-    
+
     init?(dictionary: [String: Any]) {
         guard let myID = dictionary["id"] as? String else {
             print(#function, "Unable to get ID from JSON")
             return nil
         }
-        
+
         guard let myDescription = dictionary["description"] as? String else {
             print(#function, "Unable to get description from JSON")
             return nil
         }
-        
+
         guard let myTitle = dictionary["title"] as? String else {
             print(#function, "Unable to get title from JSON")
             return nil
         }
-        
+
         guard let myLocation = dictionary["location"] as? String else {
             print(#function, "Unable to get location from JSON")
             return nil
@@ -51,7 +51,7 @@ struct RenovateProject: Codable , Identifiable {
             print(#function, "Unable to get langtitude from JSON")
             return nil
         }
-        
+
         guard let myLat = dictionary["lat"] as? Double else {
             print(#function, "Unable to get latitude from JSON")
             return nil
@@ -61,69 +61,91 @@ struct RenovateProject: Codable , Identifiable {
             print(#function, "Unable to get images from JSON")
             return nil
         }
-        
+
         guard let myOwnerID = dictionary["ownerID"] as? String else {
             print(#function, "Unable to get owner ID from JSON")
             return nil
         }
-        
+
         guard let myCategory = dictionary["category"] as? String else {
             print(#function, "Unable to get category from JSON")
             return nil
         }
-        
+
         guard let myInvestmentNeeded = dictionary["investmentNeeded"] as? Double else {
             print(#function, "Unable to get investmentNeeded from JSON")
             return nil
         }
-        
+
         guard let mySelectedInvestmentSuggestionID = dictionary["selectedInvestmentSuggestionID"] as? String else {
             print(#function, "Unable to get selectedInvestmentSuggestionID from JSON")
             return nil
         }
-        
 
         guard let myStatus = dictionary["status"] as? String else {
             print(#function, "Unable to get status from JSON")
             return nil
         }
-        
+
         guard let myStartDate = dictionary["startDate"] as? Date else {
             print(#function, "Unable to get startDate from JSON")
             return nil
         }
-        
+
         guard let myEndDate = dictionary["endDate"] as? Date else {
             print(#function, "Unable to get endDate from JSON")
             return nil
         }
-        
+
+        guard let myNumberOfBedrooms = dictionary["numberOfBedrooms"] as? Int else {
+            print(#function, "Unable to get numberOfBedrooms from JSON")
+            return nil
+        }
+
+        guard let myNumberOfBathrooms = dictionary["numberOfBathrooms"] as? Int else {
+            print(#function, "Unable to get numberOfBathrooms from JSON")
+            return nil
+        }
+
+        guard let myPropertyType = dictionary["propertyType"] as? String else {
+            print(#function, "Unable to get propertyType from JSON")
+            return nil
+        }
+
+        guard let mySquareFootage = dictionary["squareFootage"] as? Double else {
+            print(#function, "Unable to get squareFootage from JSON")
+            return nil
+        }
+
+        guard let myIsFurnished = dictionary["isFurnished"] as? Bool else {
+            print(#function, "Unable to get isFurnished from JSON")
+            return nil
+        }
+
         guard let myCreatedDate = dictionary["createdDate"] as? Date else {
             print(#function, "Unable to get createdDate from JSON")
             return nil
         }
-        
+
         guard let myUpdatedDate = dictionary["updatedDate"] as? Date else {
             print(#function, "Unable to get updatedDate from JSON")
             return nil
         }
-        
+
         guard let myFavoriteCount = dictionary["favoriteCount"] as? Int else {
             print(#function, "Unable to get favoriteCount from JSON")
             return nil
         }
-        
+
         guard let myRealtorID = dictionary["realtorID"] as? String else {
             print(#function, "Unable to get realtorID from JSON")
             return nil
         }
 
-        self.init(projectID: myID, title: myTitle, description: myDescription, location: myLocation, lng: myLng, lat: myLat, images: myImages, ownerID: myOwnerID, category: myCategory, investmentNeeded: myInvestmentNeeded, selectedInvestmentSuggestionID: mySelectedInvestmentSuggestionID, status: myStatus, startDate: myStartDate, endDate: myEndDate, createdDate: myCreatedDate, updatedDate: myUpdatedDate, favoriteCount: myFavoriteCount, realtorID: myRealtorID)
+        self.init(projectID: myID, title: myTitle, description: myDescription, location: myLocation, lng: myLng, lat: myLat, images: myImages, ownerID: myOwnerID, category: myCategory, investmentNeeded: myInvestmentNeeded, selectedInvestmentSuggestionID: mySelectedInvestmentSuggestionID, status: myStatus, startDate: myStartDate, endDate: myEndDate, numberOfBedrooms: myNumberOfBedrooms, numberOfBathrooms: myNumberOfBathrooms, propertyType: myPropertyType, squareFootage: mySquareFootage, isFurnished: myIsFurnished, createdDate: myCreatedDate, updatedDate: myUpdatedDate, favoriteCount: myFavoriteCount, realtorID: myRealtorID)
     }
-    
-    
-    
-    init(projectID: String, title: String, description: String, location: String, lng: Double, lat: Double, images: [Data], ownerID: String, category: String, investmentNeeded: Double,  selectedInvestmentSuggestionID: String, status: String, startDate: Date, endDate: Date, createdDate: Date, updatedDate: Date, favoriteCount: Int, realtorID: String) {
+
+    init(projectID: String, title: String, description: String, location: String, lng: Double, lat: Double, images: [Data], ownerID: String, category: String, investmentNeeded: Double, selectedInvestmentSuggestionID: String?, status: String, startDate: Date, endDate: Date, numberOfBedrooms: Int, numberOfBathrooms: Int, propertyType: String, squareFootage: Double, isFurnished: Bool, createdDate: Date, updatedDate: Date, favoriteCount: Int, realtorID: String) {
         self.id = projectID
         self.title = title
         self.description = description
@@ -138,12 +160,14 @@ struct RenovateProject: Codable , Identifiable {
         self.status = status
         self.startDate = startDate
         self.endDate = endDate
+        self.numberOfBedrooms = numberOfBedrooms
+        self.numberOfBathrooms = numberOfBathrooms
+        self.propertyType = propertyType
+        self.squareFootage = squareFootage
+        self.isFurnished = isFurnished
         self.createdDate = createdDate
         self.updatedDate = updatedDate
         self.favoriteCount = favoriteCount
         self.realtorID = realtorID
     }
-    
-  
-    
 }
--- UpgradeUpsell/Views/Owner/CreateProjectView.swift
+++ UpgradeUpsell/Views/Owner/CreateProjectView.swift
@@ -4,17 +4,13 @@
 //
 //  Created by Created by Zahra Shahin - Golnaz Chehrazi .
 //
-
 import SwiftUI
 import Firebase
-
 
 struct CreateProjectView: View {
     @EnvironmentObject var dbHelper: FirestoreController
     @EnvironmentObject var authHelper: FireAuthController
     @Environment(\.presentationMode) var presentationMode
-    
-//    @State private var userProjects: [RenovateProject] = []
 
     @State private var title = ""
     @State private var description = ""
@@ -26,32 +22,49 @@ struct CreateProjectView: View {
     @State private var status = ""
     @State private var startDate = Date()
     @State private var endDate = Date()
+    @State private var numberOfBedrooms = 0
+    @State private var numberOfBathrooms = 0
+    @State private var images: [Image] = []
+    @State private var propertyType = ""
+    @State private var squareFootage: Double = 0.0
+    @State private var isFurnished = false
+
+    @State private var showAlert = false
+
     
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("Property Information")) {
-                    TextField("Title", text: $title)
-                    TextField("Description", text: $description)
-                    TextField("Location", text: $location)
-                    TextField("Longitude", value: $lng, formatter: NumberFormatter())
-                    TextField("Latitude", value: $lat, formatter: NumberFormatter())
-                    TextField("Category", text: $category)
-                    TextField("Investment Needed", value: $investmentNeeded, formatter: NumberFormatter())
-                    TextField("Status", text: $status)
-                }
-                Section(header: Text("Dates")) {
-                    DatePicker("Start Date", selection: $startDate, displayedComponents: .date)
-                    DatePicker("End Date", selection: $endDate, displayedComponents: .date)
-                }
-                
-                Section {
+            ScrollView {
+                VStack(spacing: 20) {
+                    Section(header: Text("Property Information")) {
+                        TextField("Title", text: $title)
+                        TextField("Description", text: $description)
+                        TextField("Location", text: $location)
+                        TextField("Longitude", value: $lng, formatter: NumberFormatter())
+                        TextField("Latitude", value: $lat, formatter: NumberFormatter())
+                        TextField("Category", text: $category)
+                        TextField("Investment Needed", value: $investmentNeeded, formatter: NumberFormatter())
+                        TextField("Status", text: $status)
+                    }
+
+                    Section(header: Text("Details")) {
+                        Stepper("Number of Bedrooms: \(numberOfBedrooms)", value: $numberOfBedrooms, in: 0...10)
+                        Stepper("Number of Bathrooms: \(numberOfBathrooms)", value: $numberOfBathrooms, in: 0...10)
+                        TextField("Property Type", text: $propertyType)
+                        TextField("Square Footage", value: $squareFootage, formatter: NumberFormatter())
+                        Toggle("Is Furnished", isOn: $isFurnished)
+                    }
+
+                    Section(header: Text("Dates")) {
+                        DatePicker("Start Date", selection: $startDate, displayedComponents: .date)
+                        DatePicker("End Date", selection: $endDate, displayedComponents: .date)
+                    }
+
                     Button(action: {
-                        guard let userID = authHelper.user?.uid else {
-                                                  // Handle the case when the user is not signed in.
-                                                  // You can show an alert or take appropriate action.
-                                                  return
-                                              }
+                    
+                        guard let userID = dbHelper.userProfile?.id else {
+                            return
+                        }
                         
                         let newProperty = RenovateProject(
                             projectID: UUID().uuidString,
@@ -68,26 +81,64 @@ struct CreateProjectView: View {
                             status: status,
                             startDate: startDate,
                             endDate: endDate,
+                            numberOfBedrooms: numberOfBedrooms,
+                            numberOfBathrooms: numberOfBathrooms,
+                            propertyType: propertyType,
+                            squareFootage: squareFootage,
+                            isFurnished: isFurnished,
                             createdDate: Date(),
                             updatedDate: Date(),
                             favoriteCount: 0,
-                            realtorID: "" // Set the realtorID as needed
+                            realtorID: ""
                         )
-                        
+
                         dbHelper.addProperty(newProperty, userID: userID) { success in
                             if success {
-                                // Property saved successfully
+                                showAlert = true
                                 presentationMode.wrappedValue.dismiss()
+                                resetFormFields() // Reset form fields
                             } else {
                                 // Handle the case when there is an error saving the property.
                             }
                         }
+                       
                     }) {
                         Text("Add Property")
+                            .font(.title)
+                            .padding(.vertical, 10)
+                            .padding(.horizontal, 20)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    } .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Success"),
+                            message: Text("Property saved successfully"),
+                            dismissButton: .default(Text("OK"))
+                        )
                     }
                 }
+                .padding()
+                
             }
         }
     }
-}
 
+    private func resetFormFields() {
+        title = ""
+        description = ""
+        location = ""
+        lng = 0.0
+        lat = 0.0
+        category = ""
+        investmentNeeded = 0.0
+        status = ""
+        startDate = Date()
+        endDate = Date()
+        numberOfBedrooms = 0
+        numberOfBathrooms = 0
+        propertyType = ""
+        squareFootage = 0.0
+        isFurnished = false
+    }
+}
