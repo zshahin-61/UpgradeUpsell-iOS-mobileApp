aa77b6e7f0ff30158f53105410743c3d715f22c4
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-10 15:14:51 -0400

    update

--- UpgradeUpsell/Views/Investor/MakeOffers_InvestorView.swift
+++ UpgradeUpsell/Views/Investor/MakeOffers_InvestorView.swift
@@ -7,16 +7,17 @@
 import SwiftUI
 
 struct MakeOffers_InvestorView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     @EnvironmentObject var dbHelper: FirestoreController
     @EnvironmentObject var authHelper: FireAuthController
-
     
     let project: RenovateProject
         //@State private var investorID = ""
         //@State private var ownerID = ""
         //@State private var projectID = ""
-        @State private var amountOffered = 0.0
-        @State private var durationWeeks = 0
+        @State private var amountOffered = ""
+        @State private var durationWeeks = ""
         @State private var description = ""
         //@State private var status = ""
 
@@ -33,7 +34,7 @@ struct MakeOffers_InvestorView: View {
                 }
                 Button("Submit") {
                     
-                    var newOffer : InvestmentSuggestion = InvestmentSuggestion(id: UUID().uuidString, investorID: self.authHelper.user?.uid ?? "", ownerID: project.ownerID, projectID: project.id!, amountOffered: amountOffered, durationWeeks: durationWeeks, description: description, status: "New")
+                    var newOffer : InvestmentSuggestion = InvestmentSuggestion(id: UUID().uuidString, investorID: self.authHelper.user?.uid ?? "", ownerID: project.ownerID, projectID: project.id!, amountOffered: Double(amountOffered) ?? 0.0 , durationWeeks: Int(durationWeeks) ?? 0, description: description, status: "New")
                     
                     self.dbHelper.addInvestmentSuggestion(newOffer) { error in
                         if let error = error {
@@ -42,6 +43,7 @@ struct MakeOffers_InvestorView: View {
                         } else {
                             // Suggestion added successfully, you can navigate or show a confirmation message
                             print("Suggestion added successfully")
+                            self.presentationMode.wrappedValue.dismiss()
                         }
                     }
                 }
