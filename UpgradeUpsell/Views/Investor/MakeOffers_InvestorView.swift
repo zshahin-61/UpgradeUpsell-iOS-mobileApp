62397027b178357f21954dd7bc44eb75745b076b
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-10 19:06:20 -0400

    update

--- UpgradeUpsell/Views/Investor/MakeOffers_InvestorView.swift
+++ UpgradeUpsell/Views/Investor/MakeOffers_InvestorView.swift
@@ -13,13 +13,13 @@ struct MakeOffers_InvestorView: View {
     @EnvironmentObject var authHelper: FireAuthController
     
     let project: RenovateProject
-        //@State private var investorID = ""
-        //@State private var ownerID = ""
-        //@State private var projectID = ""
-        @State private var amountOffered = ""
-        @State private var durationWeeks = ""
+        
+        @State private var amountOffered = 0.0
+        @State private var durationWeeks = 0
         @State private var description = ""
-        //@State private var status = ""
+        
+        @State private var alertMessage = ""
+        @State private var showAlert = false
 
         var body: some View {
             Form {
@@ -34,20 +34,31 @@ struct MakeOffers_InvestorView: View {
                 }
                 Button("Submit") {
                     
-                    var newOffer : InvestmentSuggestion = InvestmentSuggestion(id: UUID().uuidString, investorID: self.authHelper.user?.uid ?? "", ownerID: project.ownerID, projectID: project.id!, amountOffered: Double(amountOffered) ?? 0.0 , durationWeeks: Int(durationWeeks) ?? 0, description: description, status: "New")
+                    var newOffer : InvestmentSuggestion = InvestmentSuggestion(id: UUID().uuidString, investorID: self.dbHelper.userProfile?.id ?? "", ownerID: project.ownerID, projectID: project.id!, amountOffered: Double(amountOffered) ?? 0.0 , durationWeeks: Int(durationWeeks) ?? 0, description: description, status: "New")
                     
                     self.dbHelper.addInvestmentSuggestion(newOffer) { error in
                         if let error = error {
                             // Handle error if needed
                             print("Error: \(error.localizedDescription)")
+                            alertMessage = "Error: \(error.localizedDescription)"
                         } else {
                             // Suggestion added successfully, you can navigate or show a confirmation message
                             print("Suggestion added successfully")
+                            alertMessage = "Suggestion added successfully"
+                            
                             self.presentationMode.wrappedValue.dismiss()
                         }
+                        showAlert = true
                     }
                 }
             } // FORM
+            .alert(isPresented: $showAlert) {
+                        Alert(
+                            title: Text("Conversion Result"),
+                            message: Text(alertMessage),
+                            dismissButton: .default(Text("OK"))
+                        )
+                    }
             .navigationBarTitle("Add Investment Suggestion")
         }
     }
