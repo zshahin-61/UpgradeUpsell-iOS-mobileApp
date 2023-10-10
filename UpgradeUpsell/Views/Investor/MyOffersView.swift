bb2d18111ee687f5889645ddc36c2333b27e20bf
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-10 13:17:17 -0400

    Update

---
+++ UpgradeUpsell/Views/Investor/MyOffersView.swift
@@ -0,0 +1,20 @@
+//
+//  MyOffersView.swift
+//  UpgradeUpsell
+//
+//  Created by Golnaz Chehrazi on 2023-10-10.
+//
+
+import SwiftUI
+
+struct MyOffersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyOffersView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyOffersView()
+    }
+}
---
+++ UpgradeUpsell/Views/Investor/ProjectsList_Investor_View.swift
@@ -0,0 +1,15 @@
+//
+//  ViewProjects_Investor.swift
+//  UpgradeUpsell
+//
+//  Created by Golnaz Chehrazi on 2023-10-10.
+//
+
+import SwiftUI
+
+struct ProjectsList_InvestorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
