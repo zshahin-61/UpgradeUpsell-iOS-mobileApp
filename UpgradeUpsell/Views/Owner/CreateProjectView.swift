24e108f8e73c00f91bb1d6b31bbc0b085c2f4470
zshahin-61 <zshahin.61@gmail.com>
2023-10-12 03:10:35 -0400

    p

--- UpgradeUpsell/Views/Owner/CreateProjectView.swift
+++ UpgradeUpsell/Views/Owner/CreateProjectView.swift
@@ -179,7 +179,6 @@ struct CreateProjectView: View {
                           
                         }
                         
-            
                            
                         
                         HStack {
--- UpgradeUpsell/Views/Owner/DetailView.swift
+++ UpgradeUpsell/Views/Owner/DetailView.swift
@@ -12,26 +12,56 @@ struct DetailView: View {
     
     let selectedProject: RenovateProject // Replace with your project model
 
+    @State private var title = ""
+    @State private var description = ""
+    @State private var location = ""
+    @State private var lng  = ""
+    @State private var lat = ""
+    @State private var category = ""
+    @State private var selectedCategory = ""
+    @State private var investmentNeeded = ""
+    @State private var status = ""
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numberOfBedrooms = ""
+    @State private var numberOfBathrooms = ""
+    @State private var images: [Image] = []
+    @State private var propertyType = ""
+    @State private var squareFootage = ""
+    @State private var isFurnished = false
+    
     var body: some View {
         VStack {
-            
-            
-            
-            Text("Title: \(selectedProject.title)")
-            Text("Description: \(selectedProject.description)")
-            Text("Location: \(selectedProject.location)")
-            Text("Category: \(selectedProject.category)")
-            Text("Number of Bedrooms: \(selectedProject.numberOfBedrooms)")
-            Text("Number of Bathrooms: \(selectedProject.numberOfBathrooms)")
-            
-            Text("Is Furnished: \(selectedProject.isFurnished ? "Yes" : "No")")
-            Text("Square Footage: \(selectedProject.squareFootage)")
-
-            if let imageData = selectedProject.images, let image = UIImage(data: imageData) {
-                Image(uiImage: image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 150, height: 150)
+            Group{
+                Text("Titlle:").font(.subheadline).bold()
+                
+                TextField(selectedProject.title, text: self.$title).textInputAutocapitalization(.never)
+                    .textFieldStyle(.roundedBorder)
+                
+                
+                Text("Description:").font(.subheadline).bold()
+                TextField(selectedProject.description, text: self.$description)
+                    .frame(minHeight: 100)
+                    .cornerRadius(5)
+                    .border(Color.gray, width: 0.5)
+            }
+            Group{
+                
+                
+                Text("Location: \(selectedProject.location)")
+                Text("Category: \(selectedProject.category)")
+                Text("Number of Bedrooms: \(selectedProject.numberOfBedrooms)")
+                Text("Number of Bathrooms: \(selectedProject.numberOfBathrooms)")
+                
+                Text("Is Furnished: \(selectedProject.isFurnished ? "Yes" : "No")")
+                Text("Square Footage: \(selectedProject.squareFootage)")
+                
+                if let imageData = selectedProject.images, let image = UIImage(data: imageData) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+                }
             }
         }
         .navigationBarTitle("Project Detail")
