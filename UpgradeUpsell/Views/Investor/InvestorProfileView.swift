bc0dc384b2078637d091c7a8ea607a3e086a45c4
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-16 18:24:25 -0400

    update changes

--- UpgradeUpsell/Views/Investor/InvestorProfileView.swift
+++ UpgradeUpsell/Views/Investor/InvestorProfileView.swift
@@ -16,9 +16,9 @@ struct InvestorProfileView: View {
     
     @StateObject private var photoLibraryManager = PhotoLibraryManager()
     
-    @State private var emailFromUI : String = ""
-    @State private var addressFromUI : String = ""
-    @State private var contactNumberFromUI : String = ""
+    //@State private var emailFromUI : String = ""
+    //@State private var addressFromUI : String = ""
+    //@State private var contactNumberFromUI : String = ""
     @State private var nameFromUI : String = ""
     @State private var bioFromUI : String = ""
     @State private var errorMsg : String? = nil
@@ -50,56 +50,33 @@ struct InvestorProfileView: View {
                     }
                 }
                 VStack{
-                    //Text("Picture").bold()
-                    if photoLibraryManager.isAuthorized {
-                        //HStack{
                         if let image = selectedImage {
                             Image(uiImage: image)
                                 .resizable()
                                 .frame(width: 150, height: 150)
                                 .clipShape(Circle())
                         }
-                        Button(action: {
-                            isShowingPicker = true
-                        }) {
-                            Text("Change Picture")
-                        }
-                        //}//Hstack
-                    } else {
-                        Button(action: {
-                            photoLibraryManager.requestPermission()
-                        }) {
-                            Text("Request Access For Photo Library")
-                        }
-                    }
-                }
-                .sheet(isPresented: $isShowingPicker) {
-                    if photoLibraryManager.isAuthorized {
-                        ImagePickerView(selectedImage: $selectedImage)
-                    } else {
-                        Text("Access to photo library is not authorized.")
-                    }
                 }
             }
             Group{
                 Text("Full Name:").bold()
-                TextField("Full Name:", text: self.$nameFromUI)
+                Text(self.nameFromUI)
                     .textInputAutocapitalization(.never)
                     .textFieldStyle(.roundedBorder)
-                Text("eMail:").bold()
-                Text(self.emailFromUI)
+               //Text("eMail:").bold()
+                //Text(self.$emailFromUI)
                 Text("Bio:").bold()
-                TextField("Bio", text: self.$bioFromUI)
+                Text(self.bioFromUI)
                     .textInputAutocapitalization(.never)
                     .textFieldStyle(.roundedBorder)
-                Text("Address:").bold()
-                TextField("Address", text: self.$addressFromUI)
-                    .textInputAutocapitalization(.never)
-                    .textFieldStyle(.roundedBorder)
-                Text("Phone Number:").bold()
-                TextField("Phone Number", text: self.$contactNumberFromUI)
-                    .textInputAutocapitalization(.never)
-                    .textFieldStyle(.roundedBorder)
+//                Text("Address:").bold()
+//                TextField("Address", text: self.$addressFromUI)
+//                    .textInputAutocapitalization(.never)
+//                    .textFieldStyle(.roundedBorder)
+//                Text("Phone Number:").bold()
+//                TextField("Phone Number", text: self.$contactNumberFromUI)
+//                    .textInputAutocapitalization(.never)
+//                    .textFieldStyle(.roundedBorder)
             }
             
             //                else {
@@ -114,32 +91,9 @@ struct InvestorProfileView: View {
             
             //HStack{
             Button(action: {
-                //Validate the data such as no mandatory inputs, password rules, etc.
-                //
-                dbHelper.userProfile!.address = addressFromUI
-                //Image
-                var imageData :Data? = nil
-                
-                if(selectedImage != nil )
-                {
-                    let image = selectedImage!
-                    let imageName = "\(UUID().uuidString).jpg"
-                    
-                    imageData = image.jpegData(compressionQuality: 0.1)
-                    dbHelper.userProfile!.profilePicture = imageData
-                }
-                
-                ////////
-                
-                self.dbHelper.userProfile!.fullName = nameFromUI
-                self.dbHelper.userProfile!.contactNumber = contactNumberFromUI
-                self.dbHelper.userProfile!.userBio = bioFromUI
-                
-                self.dbHelper.updateUserProfile(userToUpdate: dbHelper.userProfile!)
                 self.presentationMode.wrappedValue.dismiss()
-                //rootScreen = .Home
             }){
-                Text("Update Profile")
+                Text("Back")
             }.buttonStyle(.borderedProminent)
             
             Spacer()
@@ -156,7 +110,7 @@ struct InvestorProfileView: View {
                 self.dbHelper.getUserProfilebyUserID(userID: self.investorID){ (investorInfo, error)in
                 
                     if let error = error {
-                        //self.errorMsg = error.localizedDescription
+                        self.errorMsg = error.localizedDescription
                     }
                     else if let investorInfo = investorInfo {
                         
