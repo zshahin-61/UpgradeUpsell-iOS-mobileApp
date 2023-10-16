cc96e7459ec60b90148b12ed2db2e3ded97df221
zshahin-61 <zshahin.61@gmail.com>
2023-10-16 13:21:48 -0400

    looks better

--- UpgradeUpsell/Views/settings/ContentView.swift
+++ UpgradeUpsell/Views/settings/ContentView.swift
@@ -8,36 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @EnvironmentObject var authHelper : FireAuthController
+    @EnvironmentObject var authHelper: FireAuthController
     private var dbHelper = FirestoreController.getInstance()
-    
-    @State private var root : RootView = .Login
-    
+
+    @State private var root: RootView = .Login
+
     var body: some View {
-        
-        NavigationView{
-            switch root {
-            case .Login:
-                SignInView(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
-            case .Home:
-                HomeView(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
-            case .InvestorHome:
-                HomeView_Investor(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
-            case .RealtorHome:
-                HomeView_Realtor(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
-              
-            case .SignUp:
-                SignUpView(rootScreen: $root).environmentObject(self.authHelper).environmentObject(self.dbHelper)
-            case .Profile:
-                ProfileView(rootScreen: $root).environmentObject(self.authHelper).environmentObject(self.dbHelper)
-                
-            case .Settings:
-                SettingsView(rootScreen: $root).environmentObject(self.authHelper).environmentObject(self.dbHelper)
-}
-        }.padding()
+        ZStack { // Add a ZStack to create the background
+            Color(.systemBackground)
+              .ignoresSafeArea() // Ignore safe area edges for the background
+
+            NavigationView {
+                switch root {
+                case .Login:
+                    SignInView(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
+                case .Home:
+                    HomeView(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
+                case .InvestorHome:
+                    HomeView_Investor(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
+                case .RealtorHome:
+                    HomeView_Realtor(rootScreen: $root).environmentObject(authHelper).environmentObject(self.dbHelper)
+                case .SignUp:
+                    SignUpView(rootScreen: $root).environmentObject(self.authHelper).environmentObject(self.dbHelper)
+                case .Profile:
+                    ProfileView(rootScreen: $root).environmentObject(self.authHelper).environmentObject(self.dbHelper)
+                case .Settings:
+                    SettingsView(rootScreen: $root).environmentObject(self.authHelper).environmentObject(self.dbHelper)
+                }
+            }.padding()
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
