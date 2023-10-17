deaf6f09c68a0c6024783762b047c0f15f637435
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-17 10:13:06 -0400

    up

--- UpgradeUpsell/Views/Owner/ProjectOffersView.swift
+++ UpgradeUpsell/Views/Owner/ProjectOffersView.swift
@@ -10,7 +10,7 @@ struct ProjectOffersView: View {
     @State private var isLoading: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 10) {
+        VStack(alignment: .leading, spacing: 5) {
             List {
                 if dbHelper.userProfile == nil {
                     Text("No user login")
@@ -59,27 +59,35 @@ struct ProjectOffersView: View {
                             HStack {
                                 Text("\(suggestions[index].description)")
                             }
+                            
+                        }
+                        //.border(Color.green, width: 2)
+                        //.shadow(color: Color.gray.opacity(0.5), radius: 0.5)
+                        // .background(Color.gray.opacity(0.5) )
+                        Divider()
+                    }
+                    
+                }
+            } //List
+           // .padding()
+            HStack(alignment: .center){
+                Spacer()
+                Button(action: {
+                    // Save the updated statuses to the database for each suggestion
+                    for suggestion in suggestions {
+                        // Update the status in the database
+                        // You need to implement the Firestore update logic here.
+                        self.dbHelper.updateInvestmentStatus(suggestionID: suggestion.id!, newStatus: suggestion.status) { error in
+                            if let error = error {
+                                print("Error updating status for offer: \(error)")
+                            }
                         }
                     }
-                }
+                }) {
+                    Text("Save status changes")
+                }.buttonStyle(.borderedProminent)
+                Spacer()
             }
-            .padding()
-            
-            Button(action: {
-                // Save the updated statuses to the database for each suggestion
-                for suggestion in suggestions {
-                    // Update the status in the database
-                    // You need to implement the Firestore update logic here.
-                    self.dbHelper.updateInvestmentStatus(suggestionID: suggestion.id!, newStatus: suggestion.status) { error in
-                        if let error = error {
-                            print("Error updating status for offer: \(error)")
-                        }
-                    }
-                }
-            }) {
-                Text("Save status changes")
-            }
-            
             .onAppear {
                 if let ownerID = dbHelper.userProfile?.id {
                     self.isLoading = true
