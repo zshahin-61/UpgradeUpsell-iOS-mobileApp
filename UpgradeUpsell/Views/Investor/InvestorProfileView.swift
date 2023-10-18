4ecf5fb11e326afaf43430bca4f7c388a42a1be0
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-18 19:26:52 -0400

    update rp-67

--- UpgradeUpsell/Views/Investor/InvestorProfileView.swift
+++ UpgradeUpsell/Views/Investor/InvestorProfileView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct InvestorProfileView: View {
     @Environment(\.presentationMode) var presentationMode
     
-    @EnvironmentObject var authHelper : FireAuthController
-    @EnvironmentObject var dbHelper : FirestoreController
-    var investorID : String
+    @EnvironmentObject var authHelper: FireAuthController
+    @EnvironmentObject var dbHelper: FirestoreController
+    var investorID: String
     
     @StateObject private var photoLibraryManager = PhotoLibraryManager()
     
-    @State private var name : String = ""
-    @State private var bio : String = ""
-    @State private var rating : Double = 4.5
-    @State private var company : String = ""
-    @State private var errorMsg : String? = nil
+    @State private var name: String = ""
+    @State private var bio: String = ""
+    @State private var rating: Double = 4.5
+    @State private var company: String = ""
+    @State private var errorMsg: String? = nil
     
     @State private var showAlert = false
     
@@ -29,92 +29,79 @@ struct InvestorProfileView: View {
     @State private var imageData: Data?
     
     var body: some View {
-        VStack(alignment: .leading,spacing: 10){
-            HStack{
-                Spacer()
-                if let data = imageData,
-                   let uiImage = UIImage(data: data) {
-                    if(selectedImage == nil)
-                    {
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .frame(width: 150, height: 150)
-                            .clipShape(Circle())
-                    }
-                    else{
-                        //
-                    }
-                }
-                //VStack{
-                        if let image = selectedImage {
-                            Image(uiImage: image)
-                                .resizable()
-                                .frame(width: 150, height: 150)
-                                .clipShape(Circle())
-                        }
-                //}
-                Spacer()
+        VStack(alignment: .center, spacing: 20) {
+            if let data = imageData, let uiImage = UIImage(data: data) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+            } else {
+                Image(systemName: "person.circle")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(.gray)
             }
-            VStack{
-                HStack{
-                    Text("Full Name: ").bold()
-                    Text(self.name)
-                    Spacer()
-                }
-                HStack{
-                    Text("Comapny: ").bold()
-                    Text(self.company)
-                    Spacer()
-                }
-            }
-            Group{
-                RatingView(rating: rating)
-                Text("Bio:").bold()
-                Text(self.bio)
-                    .textInputAutocapitalization(.never)
-                    .textFieldStyle(.roundedBorder)
             
-            }
-            if let err = errorMsg{
-                Text(err).foregroundColor(Color.red).bold()
-            }
+            Text(name)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Text(company)
+                .font(.headline)
+            
+            RatingView(rating: rating)
+            
             HStack{
-            Spacer()
-                Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }){
-                    Text("Back")
-                }
-                .buttonStyle(.borderedProminent)
-                .frame(width: 100, height: 50)
+                Text("Bio:")
+                    .font(.headline)
                 Spacer()
             }
-            Spacer()
+                Text(bio)
+                    .font(.body)
+               
             
-                .navigationBarTitle("Investor Profile", displayMode: .inline)
-        }.padding()
-            .onAppear(){
-                self.dbHelper.getUserProfilebyUserID(userID: self.investorID){ (investorInfo, error)in
+            if let err = errorMsg {
+                Text(err)
+                    .foregroundColor(.red)
+                    .font(.callout)
+            }
+            
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Back")
+                    .font(.headline)
+                    .frame(width: 100, height: 50)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+        .onAppear(){
+            self.dbHelper.getUserProfilebyUserID(userID: self.investorID){ (investorInfo, error)in
                 
-                    if let error = error {
-                        self.errorMsg = error.localizedDescription
-                    }
-                    else if let investorInfo = investorInfo {
-                        self.name = investorInfo.fullName
-                        self.bio = investorInfo.userBio
-                        self.company = investorInfo.company ?? ""
-                        self.rating = investorInfo.rating ?? 4.5
-                        
-                        self.errorMsg = nil
-                        
-                        // MARK: Show image from db
-                        if let imageData = investorInfo.profilePicture as? Data {
-                            self.imageData = imageData
-                        } else {
-                            print("Invalid image data format")
-                        }
+                if let error = error {
+                    self.errorMsg = error.localizedDescription
+                }
+                else if let investorInfo = investorInfo {
+                    self.name = investorInfo.fullName
+                    self.bio = investorInfo.userBio
+                    self.company = investorInfo.company ?? ""
+                    self.rating = investorInfo.rating ?? 4.5
+                    
+                    self.errorMsg = nil
+                    
+                    // MARK: Show image from db
+                    if let imageData = investorInfo.profilePicture as? Data {
+                        self.imageData = imageData
+                    } else {
+                        print("Invalid image data format")
                     }
                 }
             }
+        }
+        .navigationBarTitle("Investor Profile", displayMode: .inline)
     }
 }
+
