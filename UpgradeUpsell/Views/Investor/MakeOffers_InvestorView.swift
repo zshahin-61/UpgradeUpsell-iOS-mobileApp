3834b7fbcbca793ec828fc6559a9427ac21af10a
GolnazCh <golnaz.chehrazi@gmail.com>
2023-10-18 21:02:30 -0400

    up

--- UpgradeUpsell/Views/Investor/MakeOffers_InvestorView.swift
+++ UpgradeUpsell/Views/Investor/MakeOffers_InvestorView.swift
@@ -8,24 +8,22 @@ import SwiftUI
 
 struct MakeOffers_InvestorView: View {
     @Environment(\.presentationMode) var presentationMode
-    
+
     @EnvironmentObject var dbHelper: FirestoreController
-    //@EnvironmentObject var authHelper: FireAuthController
-    
+
     let project: RenovateProject
-        
-    @State private var amountOffered = "" // Removed the initial value
-    @State private var durationWeeks = "" // Removed the initial value
-    @State private var description = "" // Removed the initial value
 
-        
-        @State private var alertMessage = ""
-        @State private var showAlert = false
+    @State private var amountOffered = ""
+    @State private var durationWeeks = ""
+    @State private var description = ""
 
-        var body: some View {
-            VStack(alignment: .leading, spacing: 10) {
-                Section(header: Text("Property Information").bold()){
-                    //Text("Investor ID: \(dbHelper.userProfile?.id!)")
+    @State private var alertMessage = ""
+    @State private var showAlert = false
+
+    var body: some View {
+        VStack{
+            Form {
+                Section(header: Text("Property Information").font(.headline)) {
                     HStack{
                         Text("Title: ").bold()
                         Text("\(project.title)")}
@@ -35,22 +33,22 @@ struct MakeOffers_InvestorView: View {
                     }
                     HStack{
                         Text("Released date: ").bold()
-                        Text("\(dateFormatter.string(from: project.createdDate))")
+                        Text("\(formattedDate(from: project.createdDate))")
                     }
                     VStack{
                         Text("need to be done between: ").bold()
-                        Text("From: \(dateFormatter.string(from: project.startDate))")
-                        Text("To: \(dateFormatter.string(from: project.endDate))")
+                        HStack{ Text(" \(formattedDate(from: project.startDate))")
+                            Text("   to    \(formattedDate(from: project.endDate))")
+                        }
                     }
-                    HStack{
+                    VStack{
                         Text("Description: ").bold()
                         Text("\(project.description)")
                     }
                     HStack{
                         Text("Likes: ").bold()
                         Text("\(project.favoriteCount)")
-                    }
-                    HStack{
+                        Spacer()
                         Text("Status: ").bold()
                         Text("\(project.status)")
                     }
@@ -58,60 +56,76 @@ struct MakeOffers_InvestorView: View {
                         Text("Location: ").bold()
                         Text("\(project.location)")
                     }
+                    //                Text("Title: \(project.title)")
+                    //                Text("Category: \(project.category)")
+                    //                Text("Released Date: \(formattedDate(from: project.createdDate))")
+                    //                VStack{
+                    //                    Text("Need to be done between:")
+                    //                    Text("From: \(formattedDate(from: project.startDate))")
+                    //                    Text("To: \(formattedDate(from: project.endDate))")
+                    //                }
+                    //                Text("Description: \(project.description)")
+                    //                HStack{
+                    //                    Text("Likes: \(project.favoriteCount)")
+                    //                    Spacer()
+                    //                    Text("Status: \(project.status)")
+                    //                }
+                    //                Text("Location: \(project.location)")
+                }
+                
+                Section(header: Text("Make an Offer").font(.headline)) {
+                    TextField("Amount Offered", text: $amountOffered).textFieldStyle(.roundedBorder)
+                    TextField("Duration in Weeks", text: $durationWeeks).textFieldStyle(.roundedBorder)
+                    Text("Description:")
+                    TextEditor(text: $description)
+                        .frame(height: 200)
+                        .border(Color.gray, width: 1)
+                }
+            }
+            Section {
+                Button("Submit") {
+                    let newOffer = InvestmentSuggestion(
+                        id: UUID().uuidString,
+                        investorID: self.dbHelper.userProfile?.id ?? "",
+                        investorFullName: self.dbHelper.userProfile?.fullName ?? "",
+                        ownerID: project.ownerID,
+                        projectID: project.id!,
+                        projectTitle: project.title,
+                        amountOffered: Double(amountOffered) ?? 0.0,
+                        durationWeeks: Int(durationWeeks) ?? 0,
+                        description: description,
+                        status: "New",
+                        date: Date()
+                    )
                     
-                }
-                    Form{
-                        TextField("Amount Offered", text: $amountOffered)
-                        TextField("Duration in Weeks", text: $durationWeeks)
-                        //TextField("Description", text: $description)
-                        Text("Description:")
-                        TextEditor(text: $description)
-                                        .frame(height: 200) // Adjust the height as needed
-                                        .border(Color.gray, width: 1)
-                                        .padding()
-                    }
-                        //TextField("Status", text: $status)
-                HStack{
-                    Spacer()
-                    Button("Submit") {
-                        let newOffer : InvestmentSuggestion = InvestmentSuggestion(id: UUID().uuidString, investorID: self.dbHelper.userProfile?.id ?? "", investorFullName: self.dbHelper.userProfile?.fullName ?? "", ownerID: project.ownerID, projectID: project.id!, projectTitle: project.title,amountOffered: Double(amountOffered) ?? 0.0 , durationWeeks: Int(durationWeeks) ?? 0, description: description, status: "New", date: Date())
-                        
-                        self.dbHelper.addInvestmentSuggestion(newOffer) { error in
-                            if let error = error {
-                                // Handle error if needed
-                                print("Error: \(error.localizedDescription)")
-                                alertMessage = "Error: \(error.localizedDescription)"
-                            } else {
-                                // Suggestion added successfully, you can navigate or show a confirmation message
-                                print("Suggestion added successfully")
-                                alertMessage = "Suggestion added successfully"
-                                
-                                self.presentationMode.wrappedValue.dismiss()
-                            }
-                            showAlert = true
+                    self.dbHelper.addInvestmentSuggestion(newOffer) { error in
+                        if let error = error {
+                            alertMessage = "Error: \(error.localizedDescription)"
+                        } else {
+                            alertMessage = "Suggestion added successfully"
+                            self.presentationMode.wrappedValue.dismiss()
                         }
+                        showAlert = true
                     }
-                    .buttonStyle(.borderedProminent)
-                    Spacer()
                 }
-                    Spacer()
-            } // VSTACK
-            .padding()
-            .alert(isPresented: $showAlert) {
-                        Alert(
-                            title: Text("Conversion Result"),
-                            message: Text(alertMessage),
-                            dismissButton: .default(Text("OK"))
-                        )
-                    }
-            .navigationBarTitle("Add an Offer")
+                .buttonStyle(.borderedProminent)
+            }
         }
-    
-    let dateFormatter: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateStyle = .medium
-            formatter.timeStyle = .none
-            return formatter
-        }()
-    
+        .padding()
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Conversion Result"),
+                message: Text(alertMessage),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+        .navigationBarTitle("Add an Offer")
     }
+
+    func formattedDate(from date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        return dateFormatter.string(from: date)
+    }
+}
