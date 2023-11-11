9dd72d44b84f38bb86f425f9bea51e1439532d24
GolnazCh <golnaz.chehrazi@gmail.com>
2023-11-11 14:21:16 -0500

    update

--- UpgradeUpsell/Views/Investor/MyOffersView.swift
+++ UpgradeUpsell/Views/Investor/MyOffersView.swift
@@ -11,25 +11,28 @@ import SwiftUI
 struct MyOffersView: View {
     @EnvironmentObject var authHelper: FireAuthController
     @EnvironmentObject var dbHelper: FirestoreController
-
+    
     @State private var suggestions: [InvestmentSuggestion] = []
     @State private var isLoading: Bool = false
     
+    @State private var isChatEnabled: [Bool] = []
+    
     var body: some View {
         //NavigationView {
-            VStack {
-                Text("My Offers").bold().font(.title).foregroundColor(.brown)
-                    .padding(.horizontal,10)
-                if dbHelper.userProfile == nil {
-                    Text("No user logged in")
+        VStack {
+            Text("My Offers").bold().font(.title).foregroundColor(.brown)
+                .padding(.horizontal,10)
+            if dbHelper.userProfile == nil {
+                Text("No user logged in")
+            } else {
+                if isLoading {
+                    ProgressView()
                 } else {
-                    if isLoading {
-                        ProgressView()
-                    } else {
-                        List {
-                            ForEach(suggestions) { suggestion in
-                                VStack(alignment: .leading, spacing: 10) {
-                                    Text(suggestion.projectTitle)
+                    List {
+                        ForEach(suggestions.indices, id: \.self) { index in
+                            VStack(alignment: .leading, spacing: 10) {
+                                Section{
+                                    Text(suggestions[index].projectTitle)
                                         .font(.title)
                                         .bold()
                                         .foregroundColor(Color(red: 0.0, green: 0.40, blue: 0.0))
@@ -37,55 +40,67 @@ struct MyOffersView: View {
                                     HStack {
                                         Text("Offered Amount:")
                                         Spacer()
-                                        Text(String(format: "$%.2f", suggestion.amountOffered))
+                                        Text(String(format: "$%.2f", suggestions[index].amountOffered))
                                     }
-
+                                    
                                     HStack {
                                         Text("Duration:")
                                         Spacer()
-                                        Text("\(suggestion.durationWeeks) Weeks")
+                                        Text("\(suggestions[index].durationWeeks) Weeks")
                                     }
-
+                                    
                                     HStack {
                                         Text("Status:")
                                         Spacer()
-                                        Text(suggestion.status)
+                                        Text(suggestions[index].status).foregroundColor(statusColor(for: suggestions[index].status))
                                     }
-
-                                    Text(suggestion.description)
                                     
+                                    Text(suggestions[index].description)
+                                }//Section
+                                if(suggestions[index].status != "Accept"){
                                     Button(action: {
-                                                                   deleteSuggestion(suggestion) // Call the function to delete the offer
-                                                               }) {
-                                                                   Text("Delete")
-                                                                       .foregroundColor(.red)
-                                                               }
+                                        deleteSuggestion(suggestions[index]) // Call the function to delete the offer
+                                    }) {
+                                        Text("Delete")
+                                            .foregroundColor(.red)
+                                    }
                                 }
-                                .padding(10)
-                                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color(.systemBackground)))
-                                .padding(.horizontal)
-                                .listRowBackground(Color.clear)
-                            }
-                            .onDelete(perform: deleteSuggestion2)
+                        else{
+                                    //fetchChatPermissionStatus(user1: suggestion.ownerID)
+                                    if(isChatEnabled[index]){
+                                        
+                                        NavigationLink(destination: ChatView(receiverUserID: suggestions[index].ownerID).environmentObject(dbHelper)) {
+                                            Text("Chat with Investor")
+                                        }
+                                    } //if
+                                }//else
+                            }//VStack
+                            .padding(10)
+                            .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color(.systemBackground)))
+                            .padding(.horizontal)
+                            .listRowBackground(Color.clear)
                         }
-                        .padding(.vertical, 10)
+                        .onDelete(perform: deleteSuggestion2)
+                    }
+                    .padding(.vertical, 10)
+                }
+            }
+        }
+        .navigationBarTitle("My Offers", displayMode: .inline)
+        .onAppear {
+            if let investorID = dbHelper.userProfile?.id {
+                isLoading = true
+                dbHelper.getInveSuggByInvestorID(investorID: investorID) { (suggestions, error) in
+                    if let error = error {
+                        print("Error getting investment suggestions: \(error)")
+                    } else if let suggestions = suggestions {
+                        self.suggestions = suggestions
+                        self.isChatEnabled = suggestions.map{$0.status != "Accept"  ? false : fetchChatPermissionStatus(sugg: $0)  }
+                        isLoading = false
                     }
                 }
             }
-            .navigationBarTitle("My Offers", displayMode: .inline)
-            .onAppear {
-                if let investorID = dbHelper.userProfile?.id {
-                    isLoading = true
-                    dbHelper.getInveSuggByInvestorID(investorID: investorID) { (suggestions, error) in
-                        if let error = error {
-                            print("Error getting investment suggestions: \(error)")
-                        } else if let suggestions = suggestions {
-                            self.suggestions = suggestions
-                            isLoading = false
-                        }
-                    }
-                }
-            }
+        }
         //}//nav view
     }
     
@@ -112,33 +127,65 @@ struct MyOffersView: View {
     
     // Function to delete an offer
     func deleteSuggestion2(at offsets: IndexSet) {
-            for index in offsets {
-                let suggestion = suggestions[index]
-                // Implement the logic to delete the offer from your data source (e.g., Firestore)
-                 dbHelper.deleteSuggestion(suggestion) { (success, error) in
-                     if success {
-                         // Delete was successful
-                         suggestions.remove(at: index)
-                         insertNotif(suggestion, "Delete")
-                     } else if let error = error {
-                         print("Error deleting suggestion: \(error)")
-                     }
-                 }
+        for index in offsets {
+            let suggestion = suggestions[index]
+            // Implement the logic to delete the offer from your data source (e.g., Firestore)
+            dbHelper.deleteSuggestion(suggestion) { (success, error) in
+                if success {
+                    // Delete was successful
+                    suggestions.remove(at: index)
+                    insertNotif(suggestion, "Delete")
+                } else if let error = error {
+                    print("Error deleting suggestion: \(error)")
+                }
             }
         }
+    }
     
-        func deleteSuggestion(_ suggestion: InvestmentSuggestion) {
-            // Implement the logic to delete the offer from your data source (e.g., Firestore)
-            self.dbHelper.deleteSuggestion(suggestion) { (success, error) in
-                 if success {
-                     // Delete was successful
-                     insertNotif(suggestion, "Delete")
-                     if let index = suggestions.firstIndex(where: { $0.id == suggestion.id }) {
-                         suggestions.remove(at: index)
-                     }
-                 } else if let error = error {
-                     print("Error deleting suggestion: \(error)")
-                 }
-             }
+    func deleteSuggestion(_ suggestion: InvestmentSuggestion) {
+        // Implement the logic to delete the offer from your data source (e.g., Firestore)
+        self.dbHelper.deleteSuggestion(suggestion) { (success, error) in
+            if success {
+                // Delete was successful
+                insertNotif(suggestion, "Delete")
+                if let index = suggestions.firstIndex(where: { $0.id == suggestion.id }) {
+                    suggestions.remove(at: index)
+                }
+            } else if let error = error {
+                print("Error deleting suggestion: \(error)")
+            }
         }
+    }
+    
+    func statusColor(for status: String) -> Color {
+        switch status {
+        case "Pending":
+            return .yellow
+        case "Accept":
+            return .green
+        case "Declined":
+            return .red
+        default:
+            return .black
+        }
+    }
+    
+    // Function to fetch chat permission status for the current user
+    private  func fetchChatPermissionStatus(sugg: InvestmentSuggestion) -> Bool {
+        var result = false
+        dbHelper.fetchChatPermission(user1: sugg.ownerID, user2: sugg.investorID) { (permission, error) in
+            if let error = error {
+               result = false
+                return
+            }
+
+            if let permission = permission {
+               result = permission.canChat
+            } else {
+                result = false
+            }
+        }
+        return result
+    }
+    
 }
