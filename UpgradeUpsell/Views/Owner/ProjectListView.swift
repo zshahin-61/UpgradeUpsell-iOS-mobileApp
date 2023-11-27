45768e73f939df2a0422adcde7be2ee9a8e1abbc
GolnazCh <golnaz.chehrazi@gmail.com>
2023-11-27 14:15:17 -0500

    update

--- UpgradeUpsell/Views/Owner/ProjectListView.swift
+++ UpgradeUpsell/Views/Owner/ProjectListView.swift
@@ -15,6 +15,8 @@ struct ProjectListView: View {
     @State private var userProjects: [RenovateProject] = []
     @State private var selectedProject: RenovateProject?
     @State private var isShowingPicker = false
+    @State private var showDeleteAlert = false
+    @State private var selectedOffsets: IndexSet?
     
     var body: some View {
         VStack {
@@ -49,74 +51,113 @@ struct ProjectListView: View {
                         //                        }
                         //}
                     }
-                    .onDelete(perform: deleteProjects)
+                    //                    .onDelete(perform: deleteProjects)
+                    .onDelete { indexSet in
+                        
+                        let selectedProjects = indexSet.map { userProjects[$0] }
+
+                            guard selectedProjects.allSatisfy({ $0.status.lowercased() != "in progress" }) else {
+                                // Show an alert to inform the user that a project with "In Progress" status cannot be deleted
+                                showAlert(message: "Cannot delete a project with 'In Progress' status.")
+                                return
+                            }
+                        
+                        selectedOffsets = indexSet
+                        showDeleteAlert = true
+                    }
                 }
+                
                 
                 //            .padding(.horizontal, 10)
             }
             
         }
+        .alert(isPresented: $showDeleteAlert) {
+                   Alert(
+                       title: Text("Confirm Deletion"),
+                       message: Text("Are you sure you want to delete this property?"),
+                       primaryButton: .destructive(Text("Delete")) {
+                           deleteProjects()
+                       },
+                       secondaryButton: .cancel()
+                   )
+               }
         .onAppear {
-            if let userID = self.dbHelper.userProfile?.id {
-                dbHelper.getUserProjectsWithStatus(userID: userID) { projects, error in
-                    if let projects = projects {
-                        self.userProjects = projects
-                    } else if let error = error {
-                        // Handle the error
-#if DEBUG
-                        print("Error fetching user projects: \(error.localizedDescription)")
-#endif
-                    }
-                }
-            }
+            loadProjects()
         }
         .padding(.top, 10)
 //        .navigationBarTitle("List My Properties")//VStack
     }
- //Add status Delete
-    private func deleteProjects(at offsets: IndexSet) {
-        for offset in offsets {
-            let project = userProjects[offset]
-           // userProjects[offset].status = "deleted"
-                  
-            dbHelper.updateProjectStatus(project) { success in
-                if success {
+    
+    //load projects
+    private func loadProjects(){
+        if let userID = self.dbHelper.userProfile?.id {
+            dbHelper.getUserProjectsWithStatus(userID: userID) { projects, error in
+                if let projects = projects {
+                    self.userProjects = projects
+                } else if let error = error {
+                    // Handle the error
 #if DEBUG
-                    print("Project status updated to 'deleted' successfully.")
-                    #endif
-                    // Insert a notification in Firebase
-               let notification = Notifications(
-                id: UUID().uuidString,
-                   timestamp: Date(),
-                userID: project.ownerID,
-                   event: "Project Deactive",
-                details: "Project titled '\(project.title)' has been deleted By \(dbHelper.userProfile?.fullName).",
-                   isRead: false,
-                projectID: project.id!
-               )
-
-               dbHelper.insertNotification(notification) { notificationSuccess in
-                   if notificationSuccess {
-#if DEBUG
-                       print("Notification inserted successfully.")
-                       #endif
-                   } else {
-#if DEBUG
-                       print("Error inserting notification.")
-                       #endif
-                   }
-               }
-                    
-                } else {
-#if DEBUG
-                    print("Error updating project status.")
-                    #endif
+                    print("Error fetching user projects: \(error.localizedDescription)")
+#endif
                 }
             }
         }
     }
-
     
+ //Add status Delete
+    private func deleteProjects() {
+            if let selectedOffsets = selectedOffsets {
+                for offset in selectedOffsets {
+                    let project = userProjects[offset]
+
+                    dbHelper.updateProjectStatus(project) { success in
+                        if success {
+                            loadProjects()
+#if DEBUG
+                            print("Project status updated to 'deleted' successfully.")
+                            #endif
+                            // Insert a notification in Firebase
+                            let notification = Notifications(
+                                id: UUID().uuidString,
+                                timestamp: Date(),
+                                userID: project.ownerID,
+                                event: "Project Deactive",
+                                details: "Project titled '\(project.title)' has been deleted By \(dbHelper.userProfile?.fullName).",
+                                isRead: false,
+                                projectID: project.id!
+                            )
+
+                            dbHelper.insertNotification(notification) { notificationSuccess in
+                                if notificationSuccess {
+                                    #if DEBUG
+                                    print("Notification inserted successfully.")
+                                    #endif
+                                } else {
+                                    #if DEBUG
+                                    print("Error inserting notification.")
+                                    #endif
+                                }
+                            }
+
+                        } else {
+                            #if DEBUG
+                            print("Error updating project status.")
+                            #endif
+                        }
+                    }
+                }
+            }
+        }
+    
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "Cannot Delete Project", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        // Assuming you are using SwiftUI within a UIKit environment (e.g., UIViewControllerRepresentable)
+        // If you are using SwiftUI's Alert, you can modify the code accordingly
+        UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
     
     //REALLY DELETED RECORD
 //    private func deleteProjects(at offsets: IndexSet) {
