63066dd19bb8cf16f97fb8c31c50f5b72ce820c7
zshahin-61 <zshahin.61@gmail.com>
2023-11-27 19:46:08 -0500

    up

--- UpgradeUpsell/UpgradeUpsellApp.swift
+++ UpgradeUpsell/UpgradeUpsellApp.swift
@@ -10,29 +10,18 @@ import Firebase
 import FirebaseCore
 import FirebaseAuth
 import FirebaseFirestore
-
-
-//class AppDelegate: NSObject, UIApplicationDelegate {
-//  func application(_ application: UIApplication,
-//                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//    FirebaseApp.configure()
-//      
-//    
-//  }
-//}
-
-
-
+import UserNotifications
 
 @main
 struct UpgradeUpsellApp: App {
-//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     let authHelper = FireAuthController()
     @StateObject private var themeManager = ThemeManager()
     
-    init() {
-        FirebaseApp.configure()
-    }
+//    init() {
+//       // FirebaseApp.configure()
+//    }
     
     var body: some Scene {
         WindowGroup {
@@ -56,3 +45,84 @@ struct UpgradeUpsellApp: App {
     }
 }
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    let gcmMessageIDKey = "gcm.message_id"
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        if #available(iOS 10.0, *){
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions : UNAuthorizationOptions = [.alert , .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in})
+        } else {
+            let settings: UIUserNotificationSettings =
+            UIUserNotificationSettings(types: [.alert , . badge, . sound],categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        return true
+        
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable:Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print ("Message ID: \(messageID)")
+        }
+        print(userInfo)
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+}
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        let deviceToken:[String: String] = ["token": fcmToken ?? ""]
+        print("Device token:", deviceToken)
+    }
+}
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
+
+  // Receive displayed notifications for iOS 10 devices.
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    let userInfo = notification.request.content.userInfo
+
+    if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+    }
+
+    print(userInfo)
+
+    // Change this to your preferred presentation option
+    completionHandler([[.banner, .badge, .sound]])
+  }
+
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+
+    }
+
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              didReceive response: UNNotificationResponse,
+                              withCompletionHandler completionHandler: @escaping () -> Void) {
+    let userInfo = response.notification.request.content.userInfo
+
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID from userNotificationCenter didReceive: \(messageID)")
+    }
+
+    print(userInfo)
+
+    completionHandler()
+  }
+}
